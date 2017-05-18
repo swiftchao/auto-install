2110f24846957e767bbc97b23953f0fb45412535
chaofei <chaofeibest@163.com>
2017-05-19 04:30:01 +0800

    修改生成日志的名称格式，修改日志的默认值格式，增加SOFT_HOME的变量及其默认值得设置

--- bin/auto-install.sh
+++ bin/auto-install.sh
@@ -5,10 +5,34 @@
 # Created Time: 2016-11-16 13:27:30
 #########################################################################
 #!/bin/bash
+set -e
+  
+function get_soft_home() {
+  if [ -z "${SOFT_HOME}" ]; then
+    CURRENT_DIR=`pwd`
+		SOFT_HOME="${CURRENT_DIR}/.."
+  fi
+	echo "${SOFT_HOME}"
+}
 
-CURRENT_DIR=`pwd`
-if [ -f "${CURRENT_DIR}/functions.sh" ]; then
-  source "${CURRENT_DIR}/functions.sh" 
-fi
+function load_args_file() {
+  if [ -f "${1}" ]; then
+    source "${1}"
+  fi
+}
+
+SOFT_HOME=`get_soft_home`
+
+load_args_file "${SOFT_HOME}/conf/config.conf"
+load_args_file "${SOFT_HOME}/bin/functions.sh"
 
 init_log
+
+START_TIME=$(date +%s)
+echo "`generate_timestamp` Begin..." >> "${LOGS_DIR}/${LOG_INFO_NAME}"
+
+END_TIME=$(date +%s)
+TOOK_TIME=$((START_TIME - END_TIME))
+
+echo "`generate_timestamp` Total took:[$TOOK_TIME] seconds." >> "${LOGS_DIR}/${LOG_INFO_NAME}"
+echo "`generate_timestamp` End" >> "${LOGS_DIR}/${LOG_INFO_NAME}"
--- bin/functions.sh
+++ bin/functions.sh
@@ -11,27 +11,29 @@ if [ "${DEBUG}" == "true" ]; then
   set -x
 fi
 
-CURRENT_DIR=`pwd`
-
 function load_args_config() {
   if [ -f "${1}" ]; then
     source "${1}"
   fi
 }
 
-load_args_config "${CURRENT_DIR}/../conf/config.conf"
-
 function generate_timestamp() {
   CURRENT_TIME=`date +"%Y-%m-%d %H:%M:%S"`
   echo "${CURRENT_TIME}"
+}
+
+function get_current_user() {
+  CURRENT_USER=`whoami`
+	echo "${CURRENT_USER}"
 }
 
 function generate_default_log_name() {
   if [ -z "${SOFT_NAME}" ]; then
     SOFT_NAME="${0}"
   fi
-  CURRENT_YMD_TIME=`date +"%Y-%m-%d"`
-  LOG_NAME="${SOFT_NAME}-${CURRENT_YMD_TIME}"
+	if [ -z "${LOG_NAME}" ]; then
+		LOG_NAME="${SOFT_NAME}"
+	fi
   echo "${LOG_NAME}"
 }
 
@@ -58,25 +60,30 @@ function create_args_ln_file() {
 
 function init_log() {
   if [ -z "${LOGS_DIR}" ]; then
-    LOGS_DIR="${CURRENT_DIR}/../logs"
+    LOGS_DIR="${SOFT_HOME}/logs"
   fi
-  if [ -z "${LOG_NAME}" ]; then
+	if [ -z "${LOG_NAME}" ]; then
     LOG_NAME=`generate_default_log_name`
   fi
+  CURRENT_YMD_TIME=`date +"%Y-%m-%d"`
+	CURRENT_USER=`get_current_user`
   if [ -z "$LOG_INFO_NAME" ]; then
-    LOG_INFO_NAME="${LOG_NAME}.INFO"
+    LOG_INFO_NAME="${LOG_NAME}"
   fi
+	LOG_INFO_NAME="${LOG_INFO_NAME}.${CURRENT_USER}.${CURRENT_YMD_TIME}.INFO"
   if [ -z "$LOG_WARING_NAME" ]; then
-    LOG_WARING_NAME="${LOG_NAME}.WARING"
+    LOG_WARING_NAME="${LOG_NAME}"
   fi
+	LOG_WARING_NAME="${LOG_WARING_NAME}.${CURRENT_USER}.${CURRENT_YMD_TIME}.WARING"
   if [ -z "$LOG_ERROR_NAME" ]; then
-    LOG_ERROR_NAME="${LOG_NAME}.ERROR"
+    LOG_ERROR_NAME="${LOG_NAME}"
   fi
+	LOG_ERROR_NAME="${LOG_ERROR_NAME}.${CURRENT_USER}.${CURRENT_YMD_TIME}.ERROR"
   create_dir "${LOGS_DIR}"
   create_file "${LOGS_DIR}/${LOG_INFO_NAME}"
   create_file "${LOGS_DIR}/${LOG_WARING_NAME}"
   create_file "${LOGS_DIR}/${LOG_ERROR_NAME}"
-  create_args_ln_file "${LOGS_DIR}/${LOG_INFO_NAME}" "${LOGS_DIR}/${SOFT_NAME}.INFO"
-  create_args_ln_file "${LOGS_DIR}/${LOG_WARING_NAME}" "${LOGS_DIR}/${SOFT_NAME}.WARING"
-  create_args_ln_file "${LOGS_DIR}/${LOG_ERROR_NAME}" "${LOGS_DIR}/${SOFT_NAME}.ERROR"
+  create_args_ln_file "${LOGS_DIR}/${LOG_INFO_NAME}" "${LOGS_DIR}/${LOG_NAME}.${CURRENT_USER}.INFO"
+  create_args_ln_file "${LOGS_DIR}/${LOG_WARING_NAME}" "${LOGS_DIR}/${LOG_NAME}.${CURRENT_USER}.WARING"
+  create_args_ln_file "${LOGS_DIR}/${LOG_ERROR_NAME}" "${LOGS_DIR}/${LOG_NAME}.${CURRENT_USER}.ERROR"
 }
