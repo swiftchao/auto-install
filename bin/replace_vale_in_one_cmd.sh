714be7a3d6b68e4103e646d6276b7dc31c4d2523
chaofei <chaofeibest@163.com>
2020-07-17 17:34:22 +0800

    替换使用中的常量

--- bin/replace_vale_in_one_cmd.sh
+++ bin/replace_vale_in_one_cmd.sh
@@ -1,1 +1,1 @@
-LINE=$(grep -n -A 6 'genericswitch:7c1c-f1f3-6ca1' ml2_conf_genericswitch.ini | grep -m1 password | awk -F '-' '{print $1}'); if [ -n "$LINE" ]; then sed -i "${LINE}s/.*/password = 555555/" ml2_conf_genericswitch.ini; else echo "password = 333333"  >> ml2_conf_genericswitch.ini; fi
+FILE=ml2_conf_genericswitch.ini; LINE=$(grep -n -A 6 'genericswitch:7c1c-f1f3-6ca1' $FILE | grep -m1 password | awk -F '-' '{print $1}'); if [ -n "$LINE" ]; then sed -i "${LINE}s/.*/password = 666666/" $FILE; else echo "password = 333333" >> $FILE; fi
